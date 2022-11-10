80368a70d0396dcd178de07402336c2c08084056
Kotono <tadanotorch@gmail.com>
2022-11-11 00:34:23 +0800

    CMake install

--- build_debug.ps1
+++ build_debug.ps1
@@ -1,2 +1,3 @@
 cmake -S . -B build/Debug -DCMAKE_BUILD_TYPE="Debug"
 cmake --build build/Debug --config Debug
+cmake --install build/Debug --prefix build/publish/Debug --config Debug
--- build_release.ps1
+++ build_release.ps1
@@ -1,2 +1,3 @@
 cmake -S . -B build/Release -DCMAKE_BUILD_TYPE="Release"
 cmake --build build/Release --config Release
+cmake --install build/Release --prefix build/publish/Release --config Release
